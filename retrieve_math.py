f35bb814be2ced01a709df13e6345e36ac88b05a
Fernand Pajot <fernand@epigram.me>
2024-07-19 10:05:56 -0600

    Better embedding model.

--- retrieve_math.py
+++ retrieve_math.py
@@ -1,7 +1,25 @@
-import torch.nn.functional as F
+from typing import Dict
+
+import torch
+import numpy as np
 from transformers import AutoModel, AutoTokenizer
 
-input_texts = [
+# The model works really well with cls pooling (default) but also with mean pooling.
+def pooling(outputs: torch.Tensor, inputs: Dict,  strategy: str = 'cls') -> np.ndarray:
+    if strategy == 'cls':
+        outputs = outputs[:, 0]
+    elif strategy == 'mean':
+        outputs = torch.sum(
+            outputs * inputs["attention_mask"][:, :, None], dim=1) / torch.sum(inputs["attention_mask"])
+    else:
+        raise NotImplementedError
+    return outputs.detach().cpu().numpy()
+
+model_id = 'mixedbread-ai/mxbai-embed-large-v1'
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModel.from_pretrained(model_id).cuda()
+
+docs = [
     "Tom has a red marble, a green marble, a blue marble, and three identical yellow marbles.  How many different groups of two marbles can Tom choose?",
     "The equation x 2 + 2x = i has two complex solutions. Determine the product of their real parts.",
     "I like apples",
@@ -10,17 +28,11 @@ input_texts = [
     "The length of a rectangle is 3x + 10 feet and its width is x + 12 feet. If the perimeter of the rectangle is 76 feet, how many square feet are in the area of the rectangle?",
 ]
 
-model_path = 'Alibaba-NLP/gte-large-en-v1.5'
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModel.from_pretrained(model_path, trust_remote_code=True).cuda()
+inputs = tokenizer(docs, padding=True, return_tensors='pt')
+for k, v in inputs.items():
+    inputs[k] = v.cuda()
+outputs = model(**inputs).last_hidden_state
+embeddings = pooling(outputs, inputs, 'mean')
 
-batch_dict = tokenizer(input_texts, max_length=8192, padding=True, truncation=True, return_tensors='pt')
-batch_dict = {key: value.cuda() for key, value in batch_dict.items()}
-
-outputs = model(**batch_dict)
-embeddings = outputs.last_hidden_state[:, 0]
-
-embeddings = F.normalize(embeddings, p=2, dim=1)
-print((embeddings[1:] @ embeddings[0]).tolist())
-# mask = [True, False, True, True, True]
-# print((embeddings[mask] @ embeddings[1]).tolist())
+mask = [False, True, True, True, True, True]
+print(embeddings[mask] @ embeddings[0])
