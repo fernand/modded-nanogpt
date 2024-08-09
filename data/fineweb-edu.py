ccd346e5ec5943014a6de81246b509a0fa21a77b
Fernand Pajot <fernand@epigram.me>
2024-08-09 14:21:39 -0600

    Back to uint16 and gpt tokenizer

--- data/fineweb-edu.py
+++ data/fineweb-edu.py
@@ -28,20 +28,20 @@ def write_datafile(filename, toks):
     """
     Saves token data as a .bin file, for reading in C.
     - First comes a header with 256 int32s
-    - The tokens follow, each as a uint32
+    - The tokens follow, each as a uint16
     """
     assert len(toks) < 2**31, "token count too large" # ~2.1B tokens
     # construct the header
     header = np.zeros(256, dtype=np.int32)
     header[0] = 20240520 # magic
     header[1] = 1 # version
-    header[2] = len(toks) # number of tokens after the 256*4 bytes of header (each 2 bytes as uint32)
+    header[2] = len(toks) # number of tokens after the 256*4 bytes of header (each 2 bytes as uint16)
     # construct the tokens numpy array, if not already
-    if not isinstance(toks, np.ndarray) or not toks.dtype == np.uint32:
-        # validate that no token exceeds a uint32
+    if not isinstance(toks, np.ndarray) or not toks.dtype == np.uint16:
+        # validate that no token exceeds a uint16
         maxtok = 2**16
-        assert all(0 <= t < maxtok for t in toks), "token dictionary too large for uint32"
-        toks_np = np.array(toks, dtype=np.uint32)
+        assert all(0 <= t < maxtok for t in toks), "token dictionary too large for uint16"
+        toks_np = np.array(toks, dtype=np.uint16)
     else:
         toks_np = toks
     # write to file
@@ -73,23 +73,23 @@ os.makedirs(DATA_CACHE_DIR, exist_ok=True)
 fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train")
 
 # init the tokenizer
-enc = tiktoken.get_encoding("o200k_base")
+enc = tiktoken.get_encoding("gpt2")
 eot = enc._special_tokens['<|endoftext|>'] # end of text token
 def tokenize(doc):
-    # tokenizes a single document and returns a numpy array of uint32 tokens
+    # tokenizes a single document and returns a numpy array of uint16 tokens
     tokens = [eot] # the special <|endoftext|> token delimits all documents
     tokens.extend(enc.encode_ordinary(doc["text"]))
     tokens_np = np.array(tokens)
-    assert (0 <= tokens_np).all() and (tokens_np < 2**32).all(), "token dictionary too large for uint32"
-    tokens_np_uint32 = tokens_np.astype(np.uint32)
-    return tokens_np_uint32
+    assert (0 <= tokens_np).all() and (tokens_np < 2**32).all(), "token dictionary too large for uint16"
+    tokens_np_uint16 = tokens_np.astype(np.uint16)
+    return tokens_np_uint16
 
 # tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
 nprocs = max(1, os.cpu_count() - 2) # don't hog the entire system
 with mp.Pool(nprocs) as pool:
     shard_index = 0
     # preallocate buffer to hold current shard
-    all_tokens_np = np.empty((args.shard_size,), dtype=np.uint32)
+    all_tokens_np = np.empty((args.shard_size,), dtype=np.uint16)
     token_count = 0
     for tokens in pool.imap(tokenize, fw, chunksize=16):
         # is there enough space in the current shard for the new tokens?
