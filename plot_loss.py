5aa236f9b8e5483e486bd261795dda2dfa679941
Fernand Pajot <fernand@epigram.me>
2024-09-24 11:10:22 -0600

    .

--- plot_loss.py
+++ plot_loss.py
@@ -5,8 +5,6 @@ TOKENS_PER_STEP = 524288
 if __name__ == '__main__':
     logfiles = [
         'pylog124M/e9067b07-f2e0-4b0d-bfbe-ca25a22e60f0.log', # vanilla, fineweb
-        # 'pylog124M/ee8b9c4c-9630-4651-9b22-1b26c2ec7e75.log', # vanilla, fineweb-edu
-        # 'pylog124M_l1_loss/0a4aeefd-0601-49a4-a178-f2ad7b6e1a8b.log', # fineweb-edu l = 1.0 on mlp out, ReLU
     ]
     plot_eval = False
     plot_start = True
