baa0558cbcef8bc66a9ebaf7d1aa186ef829f4ec
Fernand Pajot <fernand@epigram.me>
2024-09-24 11:09:59 -0600

    Back to fineweb since it's shuffled out of the box.

--- plot_loss.py
+++ plot_loss.py
@@ -5,9 +5,11 @@ TOKENS_PER_STEP = 524288
 if __name__ == '__main__':
     logfiles = [
         'pylog124M/e9067b07-f2e0-4b0d-bfbe-ca25a22e60f0.log', # vanilla, fineweb
-        'pylog124M_l1_loss/0a4aeefd-0601-49a4-a178-f2ad7b6e1a8b.log', # l = 1.0 on mlp out, ReLU
+        # 'pylog124M/ee8b9c4c-9630-4651-9b22-1b26c2ec7e75.log', # vanilla, fineweb-edu
+        # 'pylog124M_l1_loss/0a4aeefd-0601-49a4-a178-f2ad7b6e1a8b.log', # fineweb-edu l = 1.0 on mlp out, ReLU
     ]
     plot_eval = False
+    plot_start = True
     xs, ys = [], []
     for logfile in logfiles:
         x, y = [], []
@@ -20,9 +22,17 @@ if __name__ == '__main__':
         xs.append(x)
         ys.append(y)
 
-    plt.figure(figsize=(10, 6))
     for x, y in zip(xs, ys):
-        plt.plot(x[500:], y[500:])
+        if plot_eval:
+            if plot_start:
+                plt.plot(x[:10], y[:10])
+            else:
+                plt.plot(x[10:], y[10:])
+        else:
+            if plot_start:
+                plt.plot(x[:500], y[:500])
+            else:
+                plt.plot(x[500:], y[500:])
     plt.xlabel('Tokens')
     plt.ylabel('Loss')
     plt.tight_layout()
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 torchrun --standalone --nproc_per_node=2 train_gpt2.py \
-    --input_bin "data/fineweb_edu_10B/fineweb_train_*.bin" \
-    --input_val_bin "data/fineweb_edu_10B/fineweb_val_*.bin" \
+    --input_bin "data/fineweb_10B/fineweb_train_*.bin" \
+    --input_val_bin "data/fineweb_10B/fineweb_val_*.bin" \
     --output_dir pylog124M_l1_loss \
     --model d12 \
     --batch_size 16 \
